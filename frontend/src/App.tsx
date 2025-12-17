7d38b8986af995d80527d149a462a810dfe457d8
Unknownflow <clarencelau88@gmail.com>
2025-12-17 22:02:25 +0800

    add: new colour theme for the website

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,50 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { blue, orange } from "@mui/material/colors";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const theme = createTheme({
     palette: {
-        primary: blue,
-        secondary: orange,
+        mode: "light",
+        primary: {
+            main: "#1890ff",
+            light: "#40a9ff",
+            dark: "#096dd9",
+            contrastText: "#ffffff",
+        },
+        secondary: {
+            main: "#531dab",
+            light: "#391085",
+            dark: "#722ed1",
+            contrastText: "#ffffff",
+        },
+        action: {
+            hover: "rgba(0, 0, 0, 0.04)",
+        },
+        background: {
+            default: "#e6f7ff",
+            paper: "#FFFFFF",
+        },
+        text: {
+            primary: "#1A1A1B",
+            secondary: "#7C7C7C",
+        },
+        success: {
+            main: "#389e0d",
+            light: "#237804",
+            dark: "#52c41a",
+        },
+        warning: {
+            main: "#d48806",
+            light: "#ad6800",
+            dark: "#faad14",
+        },
+        error: {
+            main: "#f5222d",
+            light: "#cf1322",
+            dark: "#ff4d4f",
+        },
     },
 });
 
@@ -21,6 +58,7 @@ const App: React.FC = () => {
     return (
         <div className="App">
             <ThemeProvider theme={theme}>
+                <CssBaseline />
                 <BrowserRouter>
                     <QueryClientProvider client={queryClient}>
                         <UserProvider>
