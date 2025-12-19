3b78ce189559e8449f2d8f0ff559236f5907d0e3
Unknownflow <clarencelau88@gmail.com>
2025-12-19 17:37:29 +0800

    bugfix: golang server not starting up in docker containers

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -65,7 +65,7 @@ func main() {
 	// load environment variables on startup
 	err := godotenv.Load("../.env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		fmt.Println("Error loading .env file")
 	}
 
 	database.InitDB()
