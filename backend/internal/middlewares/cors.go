a1ea81966d26b37ab5e9ad487e7b1ed76cafb985
Unknownflow <clarencelau88@gmail.com>
2025-12-20 18:22:45 +0800

    add: access control max age

--- backend/internal/middlewares/cors.go
+++ backend/internal/middlewares/cors.go
@@ -21,6 +21,7 @@ func CorsMiddleware(next http.Handler) http.Handler {
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Max-Age", "3600")
 			fmt.Println("add cors header")
 		}
 
