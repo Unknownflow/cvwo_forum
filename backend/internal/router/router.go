6d1ceaef684468519a462e20b70167ac90d199e8
Unknownflow <clarencelau88@gmail.com>
2025-12-20 15:08:48 +0800

    add: update allowcredentials on cors

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -21,7 +21,7 @@ func Setup(conn *sqlx.DB) chi.Router {
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
+		AllowCredentials: true,
 		MaxAge:           300,
 	}))
 
