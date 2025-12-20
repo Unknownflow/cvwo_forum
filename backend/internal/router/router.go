0110d0014c92f50715c7fe44cf416fb704660d27
Unknownflow <clarencelau88@gmail.com>
2025-12-20 13:32:12 +0800

    add: set up cors in go server

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -1,15 +1,30 @@
 package router
 
 import (
+	"os"
+
 	"github.com/cvwo_assignment/backend/internal/middlewares"
 	"github.com/cvwo_assignment/backend/internal/routes"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/jmoiron/sqlx"
 )
 
 func Setup(conn *sqlx.DB) chi.Router {
 	r := chi.NewRouter()
+	frontendURL := os.Getenv("FRONTEND_URL")
+
+	// Basic CORS
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{frontendURL},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
 	setUpRoutes(r, conn)
 	return r
 }
