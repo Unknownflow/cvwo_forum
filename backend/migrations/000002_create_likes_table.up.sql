454a896e891d828f5c30b4682b45b8574c0901a8
Unknownflow <clarencelau88@gmail.com>
2025-12-21 16:03:03 +0800

    add: set up db migrations

---
+++ backend/migrations/000002_create_likes_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS likes 
+(
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    post_id INT NOT NULL,	
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL,
+	FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
+)
