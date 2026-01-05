523eec1b0aec1fe302d69d1e276ea1de92c73e14
Unknownflow <clarencelau88@gmail.com>
2026-01-05 23:31:46 +0800

    update: home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,23 +1,33 @@
-import { Container, Link, Paper, Typography } from "@mui/material";
+import { Button, Container, Paper, Stack, Typography } from "@mui/material";
 import React from "react";
-import { Link as RouterLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Home: React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleRegister = () => {
+        navigate("/register");
+    };
+
+    const handleLogin = () => {
+        navigate("/login");
+    };
+
     return (
         <Container maxWidth="xs">
             <Paper elevation={12} sx={{ marginTop: 8, padding: 2 }}>
-                <Typography variant="h6">Welcome to the NUS Forum.</Typography>
-                <Typography>
-                    You can create discussion topics and relevant posts and comments on NUS Forum. To start, you can{" "}
-                    <Link component={RouterLink} to="/register" underline="hover">
-                        register
-                    </Link>{" "}
-                    for a new account. If you already have an account, you can{" "}
-                    <Link component={RouterLink} to="/login" underline="hover">
-                        log in
-                    </Link>{" "}
-                    to your own account.
-                </Typography>
+                <Stack spacing={1}>
+                    <Typography variant="h6">Welcome to the NUS Forum!</Typography>
+                    <Typography>Sign up for an account to engage in discussions with other NUS students!</Typography>
+                    <Stack direction="row" justifyContent="center" spacing={2}>
+                        <Button variant="outlined" onClick={handleRegister}>
+                            Register
+                        </Button>
+                        <Button variant="outlined" onClick={handleLogin}>
+                            Login
+                        </Button>
+                    </Stack>
+                </Stack>
             </Paper>
         </Container>
     );
